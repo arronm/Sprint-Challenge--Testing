b2e01322f6c9fd92a6de234faf6641403f94a4c0
Arron M <marshall.arron@gmail.com>
2019-06-21 08:26:43 -0700

    Add tests for POST /games

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 
 const db = require('./data/models');
 const errorRef = require('./utils/errorRef');
+const validateBody = require('./middleware/validateBody');
 
 const middleware = [
   express.json(),
@@ -22,6 +23,30 @@ server.get('/games', async (req, res) => {
   try {
     let games = await db.get();
     res.json(games);
+  } catch (error) {
+    res.status(500).json(errorRef(error));
+  }
+});
+
+const bodyShape = {
+  title: {
+    required: true,
+    type: 'string',
+  },
+  genre: {
+    required: true,
+    type: 'string',
+  },
+  releaseYear: {
+    required: false,
+    type: 'number',
+  },
+};
+
+server.post('/games', validateBody(bodyShape), async (req, res) => {
+  try {
+    const game = await db.add(req.body);
+    res.status(201).json(game);
   } catch (error) {
     res.status(500).json(errorRef(error));
   }
--- server.spec.js
+++ server.spec.js
@@ -45,5 +45,68 @@ describe('server', () => {
         .expect(200);
       expect(request.body.length).toBe(0);
     });
-  })
+  });
+
+  describe('POST /games', () => {
+    const endpoint = '/games';
+    it('should correctly insert a new user with status 201', async () => {
+      const game = {
+        title: 'Cyberpunk 2077',
+        genre: 'action',
+        releaseYear: 2077
+      };
+
+      let request = await supertest(server)
+        .post(endpoint)
+        .send(game)
+        .expect(201);
+      expect(request.body).toEqual({
+        ...game,
+        id: 1,
+      });
+
+      request = await supertest(server)
+        .get(endpoint)
+        .expect(200);
+      expect(request.body.length).toBe(1);
+    });
+
+    it('should correctly handle missing genre with status 400', async () => {
+      const request = await supertest(server)
+        .post(endpoint)
+        .send({ title: 'b' })
+        .expect(400);
+      expect(request.body).toEqual({ message: "Missing required field (genre)" });
+    });
+
+    it('should correctly handle missing title with status 400', async () => {
+      const request = await supertest(server)
+        .post(endpoint)
+        .send({ genre: 'b' })
+        .expect(400);
+      expect(request.body).toEqual({ message: "Missing required field (title)" });
+    });
+
+    it('should correctly handle wrong `genre` type with status 400', async () => {
+      const request = await supertest(server)
+        .post(endpoint)
+        .send({
+          title: 'b',
+          genre: 1,
+        })
+        .expect(400);
+        expect(request.body).toEqual({ message: "Expected type for (genre) to be string, but instead saw number" });
+    });
+
+    it('should correctly handle wrong `title` type with status 400', async () => {
+      const request = await supertest(server)
+        .post(endpoint)
+        .send({
+          title: 1,
+          genre: 'a',
+        })
+        .expect(400);
+        expect(request.body).toEqual({ message: "Expected type for (title) to be string, but instead saw number" });
+    });
+  });
 });
